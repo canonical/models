5189a992e3cbc90a040c004247895158f12514c8
Łukasz 'sil2100' Zemczak <lukasz.zemczak@canonical.com>
2024-06-19 11:46:44 +0200

    Add oracular JSONs

--- scripts/snap_seeds.py
+++ scripts/snap_seeds.py
@@ -32,8 +32,9 @@ def fetch_snaps_from_seed(release, seed, seeded_snaps):
 def add_implicitly_seeded_snaps(release, seeded_snaps):
     # Sometimes some snaps are implicitly seeded in the images. Let's add
     # them to the list.
-    implicit = {"noble":  {"snapd", "bare", "core22"},
-                "mantic": {"snapd", "bare", "core22"}}
+    implicit = {"oracular": {"snapd", "bare", "core22"},
+                "noble":    {"snapd", "bare", "core22"},
+                "mantic":   {"snapd", "bare", "core22"}}
     seeded_snaps.update(implicit.get(release, set()))
 
 def get_supported_model_series():
